8823bc4f10917441830a083a0123c9c2e7f20378
Lalith <lalithpunepalli@gmail.com>
2021-05-25 09:36:41 +0530

    Initial Commit

---
+++ Mic/MicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicApp.swift
+//  Mic
+//
+//  Created by Lalith  on 25/05/21.
+//
+
+import SwiftUI
+
+@main
+struct MicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
